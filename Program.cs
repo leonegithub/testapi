2230429c2a5e9e484abbb443337c3deba1ed8696
leonegithub <webmaster@leone.it>
2025-02-14 09:00:47 +0100

    fix: null coalescing operator

--- Program.cs
+++ Program.cs
@@ -84,7 +84,7 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         ValidIssuer = jwtSettings["Issuer"],
         ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"])),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"] ?? throw new ArgumentNullException("SecretKey"))),
         RoleClaimType = ClaimTypes.Role
     };
 });
